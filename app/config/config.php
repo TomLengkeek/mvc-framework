6f2b75f8d3d34b4bd1631deb79aaba8c478d0e97
Tom Lengkeek <330713@student.mboutrecht.nl>
2022-04-14 10:57:00 +0200

    Initial commit

---
+++ app/config/config.php
@@ -0,0 +1,17 @@
+<?php 
+
+//constants for database connection
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'magazijn');
+
+//app root
+define('APPROOT', dirname(dirname(__FILE__)));
+
+//url root
+define('URLROOT', 'http://www.mvc-330713-framework.org');
+
+//sitename
+define('SITENAME', 'MVC framework');
+?>
